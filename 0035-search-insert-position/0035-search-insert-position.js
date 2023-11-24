c2ea2eee470fdea94f0ee60b03741fcfb4f90d5b
Robin Yoon <107087958+robinyoon-dev@users.noreply.github.com>
2023-11-24 13:02:28 +0900

    Time: 53 ms (45.60%), Space: 41.9 MB (66.29%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length -1;
+    
+    while(left < right){
+        let middle = Math.round((left + right) / 2);
+        
+        if(nums[middle] === target){
+            return middle;
+        }
+        
+        if(nums[middle] < target){
+            left = middle + 1;
+        }
+        
+        if(nums[middle] > target){
+            right = middle  - 1;
+        }
+        
+    };
+    
+    if(nums[left] < target){
+        return left + 1;
+    } else{
+        return left;
+    }
+    
+    
+};
+
