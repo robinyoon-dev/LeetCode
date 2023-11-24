44733f119e0759f404d6b681d8002b2d241a68ae
Robin Yoon <107087958+robinyoon-dev@users.noreply.github.com>
2023-11-24 09:56:31 +0900

    Time: 68 ms (10.53%), Space: 43.2 MB (16.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,67 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    let obj = {
+        ")" : "(",
+        "]" : "[" ,
+        "}": "{" 
+    };
+    
+    let openedArr = ["(", "[", "{"];
+    let closedArr = [")", "]", "}"];
+    
+    let isOpened = 0;
+    
+    
+    let tempArr = [];
+    
+    
+    let arr = s.split('');
+    
+    for(let char of arr){
+        if(isOpened === 0){
+            if(closedArr.includes(char)){
+                return false;
+            }
+            if(openedArr.includes(char)){
+                isOpened++;
+                tempArr.push(char);
+            }
+        }else{
+            //isOpened >= 1 인 경우
+            
+            // ([
+            if(openedArr.includes(char)){
+                tempArr.push(char)
+                isOpened++;
+            }
+            
+            
+            
+            if(closedArr.includes(char)){
+                // ()
+                if(obj[char] === tempArr[tempArr.length -1]){
+                    tempArr.pop();
+                    isOpened--;
+                
+                }else{
+                // (]    
+                    return false;
+                }
+                        
+            }
+                        
+        }    
+    }
+    
+    console.log(tempArr);
+    if(tempArr.length === 0){
+        return true;
+    }else{
+        return false;
+    }
+    
+};
